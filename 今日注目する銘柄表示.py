4cba6a1a6f25bf50f2e46d1f9feed545844a112f
kitaken1118 <122727713+kitaken1118@users.noreply.github.com>
2023-04-01 18:27:19 +0900

    Update and rename bb_MB割ったら売る2.py to 今日注目する銘柄表示.py

---
+++ 今日注目する銘柄表示.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+import re
+import yfinance as yf
+
+st.title('テクニカル分析による注目銘柄表示')
+option_1 = st.selectbox('検索する銘柄群を選択してください',
+                        ('日経225','1~2Kで買える出来高が多い銘柄','両方'))
+st.write('You selected: 'option_1)
