8dedafba04e9f628cbe07372386fcc9705ab47c9
kitaken1118 <122727713+kitaken1118@users.noreply.github.com>
2023-04-01 18:31:04 +0900

    Update 今日注目する銘柄表示.py

--- 今日注目する銘柄表示.py
+++ 今日注目する銘柄表示.py
@@ -8,4 +8,4 @@ import yfinance as yf
 st.title('テクニカル分析による注目銘柄表示')
 option_1 = st.selectbox('検索する銘柄群を選択してください',
                         ('日経225','1~2Kで買える出来高が多い銘柄','両方'))
-st.write('You selected: 'option_1)
+st.write('You selected: 'option_1')
